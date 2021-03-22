325105d095187252e6af3ef458c53f580c7369ef
Murat Karaca <murat.github@hotmail.com>
2021-03-22 22:21:52 +0300

    fix git

--- index.js
+++ index.js
@@ -2,10 +2,8 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const priceCommand = "!price "
 const helpCommand = "!help"
-const taxCommand = '!gathertax '
-const commands = [priceCommand, helpCommand, taxCommand];
+const commands = [priceCommand, helpCommand];
 const priceSearch = require('./src/baseFunctions/priceSearch').priceSearch
-const gatherTax = require('./src/baseFunctions/gatherTax').gatherTax
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -13,7 +11,6 @@ client.on('ready', () => {
 
 client.on('message', messageObject => {
     if (messageObject.content.startsWith(priceCommand)) return priceSearch(messageObject, priceCommand);
-    if (messageObject.content.startsWith(taxCommand)) return gatherTax(messageObject, taxCommand);
     if (messageObject.content.startsWith(helpCommand)) return messageObject.reply(`Commands you can use are ${commands}`)
 });
 
