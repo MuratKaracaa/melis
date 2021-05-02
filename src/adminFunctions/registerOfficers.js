9b53839b720c1eece395715cfebee2dacc2dfa90
Murat Karaca <murat.github@hotmail.com>
2021-05-02 15:55:20 +0300

    officer reg

--- src/adminFunctions/registerOfficers.js
+++ src/adminFunctions/registerOfficers.js
@@ -2,23 +2,26 @@ const { GoogleSpreadsheet } = require('google-spreadsheet');
 const { helperFunctions } = require('../utils');
 const app = helperFunctions.initFireBase()
 const db = app.database()
+const creds = {
+    type: process.env.type,
+    project_id: process.env.projectId,
+    private_key_id: process.env.private_key_id,
+    private_key: process.env.GOOGLE_PRIVATE_KEY,
+    client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+    client_id: process.env.client_id,
+    auth_uri: process.env.auth_uri,
+    token_uri: process.env.token_uri,
+    auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
+    client_x509_cert_url: process.env.client_x509_cert_url
+}
+console.log(creds)
 
 
 const doc = new GoogleSpreadsheet(process.env.spreadSheetId);
 exports.registerOfficers = async function(messageObject) {
     async function auth() {
-        await doc.useServiceAccountAuth({
-            type: process.env.type,
-            project_id: process.env.projectId,
-            private_key_id: process.env.private_key_id,
-            private_key: process.env.GOOGLE_PRIVATE_KEY,
-            client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
-            client_id: process.env.client_id,
-            auth_uri: process.env.auth_uri,
-            token_uri: process.env.token_uri,
-            auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
-            client_x509_cert_url: process.env.client_x509_cert_url
-        })
+        await doc.useServiceAccountAuth(creds)
+
     }
     auth()
 
